2f540de0d20a02bfb686b7b44502e03a53a664b8
eralmione <eralmi@outlook.de>
2019-09-27 11:18:56 +0200

    Settings

---
+++ src/dataLayer/settings/PersistenceSettings.java
@@ -0,0 +1,13 @@
+package dataLayer.settings;
+
+public class PersistenceSettings {
+	private String type;
+
+	public String getPersistenceType() {
+		return type;
+	}
+
+	public void setPersistenceType(String type) {
+		this.type = type;
+	}
+}
